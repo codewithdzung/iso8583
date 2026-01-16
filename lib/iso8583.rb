a66660d445086c10fad3ceea8482c644c154fa0f
Nguyen Tien Dzung <dzung.nguyentien@every-pay.com>
2026-01-16 12:12:47 +0700

    Initial commit: iso8583

---
+++ lib/iso8583.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "iso8583/version"
+
+module Iso8583
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/iso8583/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Iso8583
+  VERSION = "0.1.0"
+end
